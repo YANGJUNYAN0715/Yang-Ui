cd594dda5fe3eaed6e74068f40717688550bf3c9
Junyan Yang <1310204834@qq.com>
2022-08-18 16:40:03 +0800

    2022.8.18

---
+++ Calendar/src/components/utils.js
@@ -0,0 +1,7 @@
+function getFirstWeekDay(year, month) {
+  const date = new Date(year, month - 1, 1);
+  return date.getDate();
+}
+function getMonthDayCount(year, month) {
+  const date = new Date();
+}
